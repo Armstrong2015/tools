aa2a37628b12cafe660e2b1615b314066e4b9cbd
hichens <49480036+hehichens@users.noreply.github.com>
2020-07-12 18:19:50 +0800

    2020

---
+++ bin/_autorun.cmd
@@ -0,0 +1,9 @@
+@echo off
+
+call "C:\home\hichens\clink\0.4.9\clink" inject --profile "~\clink
+
+prompt [Hichens]$$$S
+
+cd C:\home
+doskey /macrofile=c:\local\bin\_doskey
+
--- readme.txt
+++ readme.txt
@@ -1,2 +1,7 @@
 # this is my tools 
-
+- cnblogs: 博客园皮肤
+- 爬虫： 爬取一些小玩意儿
+- _vimrc: vim配置文件
+- bin: windows10 命令行配置
+- chrome_extension: chrome插件不翻墙下载
+- condarc.txt: conda配置文件
